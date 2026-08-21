599aca515a659fdd54ebe6b6123d22ddd9f94e05
karthick subramanian <85538590+karthick1000@users.noreply.github.com>
2026-08-22 00:14:28 +0530

    Revert "Fix Vercel middleware crash: run in Node.js runtime"
    This reverts commit 56fa3cf837db389c8292c6bc6ccb53cb733b01ec.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error nodeMiddleware is not yet in the TS types but is valid in Next.js 15.3+
-    nodeMiddleware: true,
-  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
